6bc8b64fe35c6b3185356b5cee63e16246fbb056
Abdullah <hasan.techtomorrow@gmail.com>
2023-05-15 16:00:31 +0600

    Client side route created for product .

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -5,6 +5,11 @@ import Home from '../components/Home.vue';
 import CategoryList from '../components/pages/category/index.vue';
 import CreateCategory from '../components/pages/category/create.vue';
 import EditCategory from '../components/pages/category/edit.vue';
+
+import ProductList from '../components/pages/product/index.vue';
+import CreateProduct from '../components/pages/product/create.vue';
+import EditProduct from '../components/pages/product/edit.vue';
+
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
@@ -37,6 +42,29 @@ const  routes = new VueRouter({
         
         path: '/Category/edit/:id',
         component: EditCategory,
+        name: 'edit-category',
+      }
+      ,
+
+      {
+        path:'/product',
+        component:ProductList,
+        name:'product',
+        
+        
+      },
+
+      {
+        
+        path: '/product/create',
+        component: CreateProduct,
+        name: 'create-category',
+      }
+      ,
+      {
+        
+        path: '/product/edit/:id',
+        component: EditProduct,
         name: 'edit-category',
       }
     ]
